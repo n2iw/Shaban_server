bbf4543ee118666ad82a3cad6150e193bff53803
Ming Ying <yingming18@gmail.com>
2017-03-03 20:19:56 -0500

    Set production port to 8080

--- config/env/production.js
+++ config/env/production.js
@@ -19,13 +19,13 @@ module.exports = {
 
    models: {
       connection: 'MysqlProduction'
-   }
+   },
 
   /***************************************************************************
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  // port: 80,
+   port: 8080
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
