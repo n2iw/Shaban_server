e04e34897702b559c7518a823a36a07bfe5e2bc5
Ming Ying <yingming18@gmail.com>
2017-06-07 16:53:34 -0400

    Fix Class name in comments

--- api/controllers/GroupsController.js
+++ api/controllers/GroupsController.js
@@ -1,5 +1,5 @@
 /**
- * GroupController
+ * GroupsController
  *
  * @description :: Server-side logic for managing groups
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
