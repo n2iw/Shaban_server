d8e89a5c5266b0bfd84fde4fdb6887059395a146
Ming Ying <yingming18@gmail.com>
2017-06-15 16:14:59 -0400

    Add confirm popup for all delete buttons.

---
+++ assets/js/confirm-delete.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $( ".delete-item" ).submit(function( event ) {
+        return confirm("!!!!!!!!!!!!!!!!!\nYou are going to DELETE this item!\nAre you sure you want to do it?");
+  });
+});
